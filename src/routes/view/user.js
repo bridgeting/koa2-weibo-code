a1df49dd9b83a2f217b00b52ecb4feb1aaaeee7f
bridget <jiali.ding@outlook.com>
2021-10-22 18:01:21 +0800

    fix: fix login status on login page

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -2,14 +2,34 @@
  * @description user view route
  */
 
+const { getUserInfo } = require('../../service/user')
+
 const router = require('koa-router')()
 
+/**
+ * get userInfo from redis session userInfo
+ * @param {Object} ctx 
+ */
+function getLoginInfo(ctx) {
+    let data = {
+        isLogin: false
+    }
+    const userInfo = ctx.session.userInfo
+    if (userInfo) {
+        data = {
+            isLogin: true,
+            userName: userInfo.userName
+        }
+    }
+    return data
+}
+
 router.get('/login', async (ctx, next) => {
-    await ctx.render('login', {})
+    await ctx.render('login', getLoginInfo(ctx))
 })
 
 router.get('/register', async (ctx, next) => {
-    await ctx.render('register', {})
+    await ctx.render('register', getLoginInfo(ctx))
 })
 
 module.exports = router
